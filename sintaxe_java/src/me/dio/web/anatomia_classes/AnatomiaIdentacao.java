a29e85b7196a38ba9dc7e27eaaa134385fd610ee
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-09 12:42:02 -0300

    Aprendendo a Sintaxe Java - Anatomia das Classes (Part. 4)

---
+++ sintaxe_java/src/me/dio/web/anatomia_classes/AnatomiaIdentacao.java
@@ -0,0 +1,30 @@
+package me.dio.web.anatomia_classes;
+
+public class AnatomiaIdentacao {
+	public static void main(String[] args) {
+	metodoSemIdentacao();
+		metodoIdentado();
+	}
+	
+	/* Exemplo de sem Identação */
+	public static void metodoSemIdentacao() {
+	int mediaFinal = 6;
+	if(mediaFinal<6)
+	System.out.println("REPROVADO");
+	else if(mediaFinal==6)
+	System.out.println("PROVA MINERVA");
+	else
+	System.out.println("APROVADO");
+	}
+	
+	/* Exemplo Identado (Facilitando a leitura do Código pelo Desenvolvedor) */
+	public static void metodoIdentado() {
+		int mediaFinal = 6;
+		if(mediaFinal<6)
+			System.out.println("REPROVADO");
+		else if(mediaFinal==6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+	}
+}
