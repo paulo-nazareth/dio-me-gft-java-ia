71b0d7058ef496e01e2f29ca14b20dcd193f2f34
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-09 12:39:18 -0300

    Aprendendo a Sintaxe Java - Anatomia das Classes (Part. 2)

---
+++ sintaxe_java/src/me/dio/web/anatomia_classes/AnatomiaDeclaracoes.java
@@ -0,0 +1,19 @@
+package me.dio.web.anatomia_classes;
+
+public class AnatomiaDeclaracoes {
+	public class Aula02 {
+
+		public static void main(String[] args) {
+			/*Declaração de Variáveis*/
+			//Exemplos de Declarações
+			int numeroZero = 0;
+			int numero_um = 1;
+			int numero$um = 2;
+			
+			//O "final" garante que não seja possível modificar, apesar das boas praticas recomendar que declarações Maiusculas (São Constrantes), não pode ser modificada
+			final String BR = "Brasil";
+			final Double PI = 3.141718;
+			int ANO_2000 = 2000;
+		}
+	}
+}
