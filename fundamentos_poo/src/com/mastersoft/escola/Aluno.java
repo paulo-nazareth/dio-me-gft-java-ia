ded01e60fea0a67c145069db485bf7132894a98f
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-09 17:19:35 -0300

    Fundamentos da Programação Orientada a Objetos - Padrão JavaBeans

--- fundamentos_poo/src/com/mastersoft/escola/Aluno.java
+++ fundamentos_poo/src/com/mastersoft/escola/Aluno.java
@@ -3,7 +3,35 @@ package com.mastersoft.escola;
 //Exemplos de Getters e Setters
 public class Aluno {
 	
-	String nome;
-	int idade;
+	//Padrão JavaBeans
+	private String nome;
+
+	private int idade;
+
+	private String sexo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 	
 }
