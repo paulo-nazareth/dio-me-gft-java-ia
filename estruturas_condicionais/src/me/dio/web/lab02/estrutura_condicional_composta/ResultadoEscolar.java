723c358e4519bc5628a41797c2a5730558a32c59
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-09 14:31:05 -0300

    Introdução e Estruturas Condicionais com Java - Condicional Composta

---
+++ estruturas_condicionais/src/me/dio/web/lab02/estrutura_condicional_composta/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+package me.dio.web.lab02.estrutura_condicional_composta;
+
+public class ResultadoEscolar {
+	
+	public static void main(String[] args) {
+		int nota = 6;
+		
+		if(nota >=7)
+			System.out.println("Aprovado");
+		else
+			System.out.println("Reprovado");
+	}
+
+}
