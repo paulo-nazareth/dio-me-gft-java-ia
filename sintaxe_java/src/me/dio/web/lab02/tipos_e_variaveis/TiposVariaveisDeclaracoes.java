9de6a4b52619dc47996234eda329b05c1512651d
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-09 12:50:22 -0300

    Aprendendo a Sintaxe Java - Tipos e Variáveis (Declarações)

---
+++ sintaxe_java/src/me/dio/web/lab02/tipos_e_variaveis/TiposVariaveisDeclaracoes.java
@@ -0,0 +1,10 @@
+package me.dio.web.lab02.tipos_e_variaveis;
+
+public class TiposVariaveisDeclaracoes {
+
+	/*
+	 * Declaração de Variáveis
+	 * <Tipo> <nomeVariavel> <atribuicaoDeValorOpcional> 
+	 * */
+
+}
