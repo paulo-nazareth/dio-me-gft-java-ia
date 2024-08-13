35df960a946534008c6596b06602444d82e4f303
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-13 17:58:13 -0300

    Criando uma API REST Documentada com Spring Web e Swagger - Rest Controller RequestMapping Prefixo

--- spring-web/src/main/java/me/dio/web/spring_web/controller/UsuarioController.java
+++ spring-web/src/main/java/me/dio/web/spring_web/controller/UsuarioController.java
@@ -9,38 +9,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.web.spring_web.model.Usuario;
 import me.dio.web.spring_web.repository.UsuarioRepository;
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 	
     @Autowired
     private UsuarioRepository repository;
 
-    @PostMapping("/usuarios")
+    @PostMapping
     public void post(@RequestBody Usuario usuario){
         repository.save(usuario);
     }
     
-    @PutMapping("/usuarios")
+    @PutMapping
     public void put(@RequestBody Usuario usuario){
         repository.update(usuario);
     }
     
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<Usuario> getAll(){
         return repository.listAll();
     }
     
-    @GetMapping("/usuario/{id}")
+    @GetMapping("/{id}")
     public Usuario getOne(@PathVariable("id") Integer id){
         return repository.finById(id);
     }
     
-    @DeleteMapping("/usuarios/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Integer id){
         repository.remove(id);
     }
