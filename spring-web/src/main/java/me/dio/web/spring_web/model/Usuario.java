4ad4fb130d09798330b3292e731a6fb502850a95
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-13 17:57:04 -0300

    Criando uma API REST Documentada com Spring Web e Swagger - Rest Controller

---
+++ spring-web/src/main/java/me/dio/web/spring_web/model/Usuario.java
@@ -0,0 +1,45 @@
+package me.dio.web.spring_web.model;
+
+public class Usuario {
+
+	private Integer id;
+	
+	private String login;
+	
+	private String password;
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", login=" + login + ", password=" + password + "]";
+	}
+	
+}
