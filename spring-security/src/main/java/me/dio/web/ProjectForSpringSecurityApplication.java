0fbe4056294b5979723ce0d3c817a5a1fe191b6f
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-14 21:11:41 -0300

    Adicionando Segurança a uma API REST com Spring Security - Autenticação Simples

---
+++ spring-security/src/main/java/me/dio/web/ProjectForSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package me.dio.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForSpringSecurityApplication.class, args);
+	}
+
+}
