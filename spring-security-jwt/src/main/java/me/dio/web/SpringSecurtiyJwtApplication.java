cb8b72029a26f90e4819ae338e800342b55bd34e
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-15 21:47:47 -0300

    Adicionando Segurança a uma API REST com Spring Security - JWT - JSON Web Token - Parte 1

---
+++ spring-security-jwt/src/main/java/me/dio/web/SpringSecurtiyJwtApplication.java
@@ -0,0 +1,13 @@
+package me.dio.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurtiyJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurtiyJwtApplication.class, args);
+	}
+
+}
