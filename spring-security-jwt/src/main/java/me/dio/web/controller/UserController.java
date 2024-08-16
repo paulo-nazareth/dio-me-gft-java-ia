186aed77dfaa17c10d4b836c7526698883158b3f
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-15 22:54:14 -0300

    Adicionando Segurança a uma API REST com Spring Security - JWT - JSON Web Token - Parte 3

---
+++ spring-security-jwt/src/main/java/me/dio/web/controller/UserController.java
@@ -0,0 +1,23 @@
+package me.dio.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.dio.web.model.User;
+import me.dio.web.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+    @Autowired
+    private UserService service;
+    
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
