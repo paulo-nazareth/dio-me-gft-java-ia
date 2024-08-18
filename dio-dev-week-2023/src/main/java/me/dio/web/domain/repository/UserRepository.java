e03f0ee3631fe442cbcf56009c119bf3c88ab99f
Paulo Nazareth <paulo.nazareth@live.com>
2024-08-18 12:59:26 -0300

    API REST na Nuvem Spring Boot 3, Java 17, Railway - Implementação da Camada de Dominio

---
+++ dio-dev-week-2023/src/main/java/me/dio/web/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package me.dio.web.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.web.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+    
+}
